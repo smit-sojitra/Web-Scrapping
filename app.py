53706501621f578f2db9f263f2fc68eef289b54c
Smit Sojitra <smitsojitra810@gmail.com>
2025-04-04 22:54:46 +0530

    MongoDb Added

--- app.py
+++ app.py
@@ -1,36 +1,41 @@
-from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS,cross_origin
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
-logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
+import pymongo
+
+logging.basicConfig(filename="scrapper.log", level=logging.INFO)
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['GET'])
+
+@app.route("/", methods=["GET"])
 def homepage():
     return render_template("index.html")
 
-@app.route("/review" , methods = ['POST' , 'GET'])
+
+@app.route("/review", methods=["POST", "GET"])
 def index():
-    if request.method == 'POST':
+    if request.method == "POST":
         try:
-            searchString = request.form['content'].replace(" ","")
+            searchString = request.form["content"].replace(" ", "")
+            print(searchString)
             flipkart_url = "https://www.flipkart.com/search?q=" + searchString
             uClient = uReq(flipkart_url)
             flipkartPage = uClient.read()
             uClient.close()
             flipkart_html = bs(flipkartPage, "html.parser")
-            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
+            bigboxes = flipkart_html.find_all("div", {"class": "cPHDOP col-12-12"})
             del bigboxes[0:3]
             box = bigboxes[0]
-            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
+            productLink = "https://www.flipkart.com" + box.div.div.div.a["href"]
             prodRes = requests.get(productLink)
-            prodRes.encoding='utf-8'
+            prodRes.encoding = "utf-8"
             prod_html = bs(prodRes.text, "html.parser")
-            print(prod_html)
-            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
+            # print(prod_html)y
+            commentboxes = prod_html.find_all("div", {"class": "RcXBOT"})
 
             filename = searchString + ".csv"
             fw = open(filename, "w")
@@ -39,48 +44,63 @@ def index():
             reviews = []
             for commentbox in commentboxes:
                 try:
-                    #name.encode(encoding='utf-8')
-                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+                    # name.encode(encoding='utf-8')
+                    name = commentbox.div.div.find_all("p", {"class": "_2NsDsF"})[
+                        0
+                    ].text
 
                 except:
                     logging.info("name")
 
                 try:
-                    #rating.encode(encoding='utf-8')
+                    # rating.encode(encoding='utf-8')
                     rating = commentbox.div.div.div.div.text
 
-
                 except:
-                    rating = 'No Rating'
+                    rating = "No Rating"
                     logging.info("rating")
 
                 try:
-                    #commentHead.encode(encoding='utf-8')
+                    # commentHead.encode(encoding='utf-8')
                     commentHead = commentbox.div.div.div.p.text
 
                 except:
-                    commentHead = 'No Comment Heading'
+                    commentHead = "No Comment Heading"
                     logging.info(commentHead)
                 try:
-                    comtag = commentbox.div.div.find_all('div', {'class': ''})
-                    #custComment.encode(encoding='utf-8')
-                    custComment = comtag[0].div.text
+                    comtag = commentbox.div.div.find_all("div", {"class": "ZmyHeo"})
+                    # custComment.encode(encoding='utf-8')
+                    custComment = comtag[0].div.div.text
                 except Exception as e:
                     logging.info(e)
 
-                mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
-                          "Comment": custComment}
+                mydict = {
+                    "Product": searchString,
+                    "Name": name,
+                    "Rating": rating,
+                    "CommentHead": commentHead,
+                    "Comment": custComment,
+                }
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
-            return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
+
+            client = pymongo.MongoClient(
+                "mongodb+srv://smit:izj32bnbPVGQpoIF@hackathon.uvuv7c1.mongodb.net/?retryWrites=true&w=majority&appName=Hackathon"
+            )
+            db = client["Flipkart_Scrapper"]
+            reviews_collection = db["reviews"]
+            reviews_collection.insert_many(reviews)
+            return render_template(
+                "result.html", reviews=reviews[0 : (len(reviews) - 1)]
+            )
         except Exception as e:
             logging.info(e)
-            return 'something is wrong'
+            return "something is wrong"
     # return render_template('results.html')
 
     else:
-        return render_template('index.html')
+        return render_template("index.html")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
